6887372cda01e84d1e6925bdae4e622ba0efdeb9
asmlima <andresoares_201@hotmail.com>
2021-10-28 09:57:06 -0300

    Delete seller TEACHER

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -9,13 +8,32 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
-        public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
@@ -33,17 +51,17 @@ namespace SalesWebMvc.Models
 
         public void AddSales(SalesRecord sr)
         {
-            SalesRecords.Add(sr);
+            Sales.Add(sr);
         }
 
         public void RemoveSales(SalesRecord sr)
         {
-            SalesRecords.Remove(sr);
+            Sales.Remove(sr);
         }
 
         public double TotalSales(DateTime initial, DateTime final)
         {
-            return SalesRecords.Where(sale => sale.Date >= initial && sale.Date <= final).Sum(sale => sale.Amount);
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
     }
 }
